e5e45aa322a653478ea2b871e96ecaa4e9c5946e
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-10 17:13:27 -0600

    'POST - /api/users' implemented

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ server.use(express.json());
 const Users = require('./data/db.js');
 
 
-// POST - /api/users
+// POST - /api/users - insert()
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
 
-// GET - /api/users
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  } else {
+    Users.insert(userInfo).then(user => {
+      res.status(201).json(user);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    });
+  }
+  res.status(200);
+});
+
+// GET - /api/users - find()
 server.get('/api/users', (req, res) => {
   Users.find().then(users => {
     res.status(200).json(users);
@@ -25,8 +40,8 @@ server.get('/api/users', (req, res) => {
   res.status(200);
 });
 
-// GET - /api/users/:id
+// GET - /api/users/:id - findById()
 
-// DELETE - /api/users/:id
+// DELETE - /api/users/:id - remove()
 
-// PUT - /api/users/:id
+// PUT - /api/users/:id - update()
