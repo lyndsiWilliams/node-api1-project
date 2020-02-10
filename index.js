cbe91797515270d25eebc2d9cea8a75280d22306
lyndsiWilliams <kcatgirl@gmail.com>
2020-02-10 17:24:45 -0600

    'DELETE - /api/users/:id' implemented

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server.get('/api/users', (req, res) => {
     res.status(200).json(users);
   }).catch(err => {
     console.log(err);
-    res.status(500).json({ errorMessage: 'The users information could not be retrieved.' });
+    res.status(500).json({ errorMessage: "The users information could not be retrieved." });
   });
   res.status(200);
 });
@@ -43,5 +43,18 @@ server.get('/api/users', (req, res) => {
 // GET - /api/users/:id - findById()
 
 // DELETE - /api/users/:id - remove()
+server.delete('/api/users/:id', (req, res) => {
+  if (!req.params.id) {
+    res.status(404).json({ message: "The user with the specified ID does not exist." })
+  } else {
+    Users.remove(req.params.id).then(removed => {
+      res.status(200).json(removed);
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    });
+  }
+  res.status(200);
+});
 
 // PUT - /api/users/:id - update()
